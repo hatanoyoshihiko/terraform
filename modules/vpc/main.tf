14204327b56d28e88d26b26140ba4155a6f37135
Hatano Yoshihiko <66893746+hatanoyoshihiko@users.noreply.github.com>
2023-01-16 21:53:49 +0900

    fixed specified route table id

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -144,14 +144,14 @@ resource "aws_route_table" "private" {
 ### public a 
 resource "aws_route_table_association" "public_a" {
   subnet_id = aws_subnet.public_a.id
-  route_table_id = aws_route.public.id
+  route_table_id = aws_route_table.public.id
 }
 
-# ### public c
-# resource "aws_route_table_association" "public_c" {
-#   subnet_id = aws_subnet.public_c.id
-#   route_table_id = aws_route.public.id
-# }
+### public c
+resource "aws_route_table_association" "public_c" {
+  subnet_id = aws_subnet.public_c.id
+  route_table_id = aws_route_table.public.id
+}
 
 ### protected
 resource "aws_route_table_association" "protected_a" {
