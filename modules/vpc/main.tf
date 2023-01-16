ed3ff98179b7c3373a2307e9470fa997f94f8dd6
Hatano Yoshihiko <66893746+hatanoyoshihiko@users.noreply.github.com>
2023-01-16 21:41:24 +0900

    updated vpc module

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,151 +1,225 @@
 # Module VPC
 
-## Create VPC
-resource "aws_vpc" "example" {
-  cidr_block           = "var.vpc.cidr"
-  enable_dns_support   = "var.vpc.dns_support"
-  enable_dns_hostnames = "var.vpc.dns_hostname"
+## Credential information
+provider "aws" {
+  shared_config_files      = local.shared_config_files
+  shared_credentials_files = local.shared_credentials_files
+  profile                  = local.profile
+  region                   = local.region
+}
+
+data "aws_caller_identity" "current" {}
+
+## VPC
+resource "aws_vpc" "vpc" {
+  cidr_block           = local.vpc_cidr
+  enable_dns_support   = local.vpc_dns_support
+  enable_dns_hostnames = local.vpc_dns_hostname
 
   tags = {
-    Name = "var.vpc.env"
+    Name = local.vpc_name
+  }
+}
+
+## Internet Gateway
+### Internet Gateway for VPC
+resource "aws_internet_gateway" "ig" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = local.ig_name
   }
 }
 
 ## Subnet for Public Network
-### Create Subnet for public a
+### Subnet for public a
 resource "aws_subnet" "public_a" {
-  vpc_id                 = aws_vpc.example.id
-  cidr_block             = "var.vpc.subnet.public_a.cidr"
-  map_public_ip_on_launc = true
-  availability_zone      = "ap-northeast-1a"
+  vpc_id                 = aws_vpc.vpc.id
+  cidr_block             = local.subnet_public_a_cidr
+  map_public_ip_on_launch = true
+  availability_zone      = local.subnet_a_az
+
+  tags = {
+    Name = local.subnet_public_name_a
+  }
 }
 
-### Create Subnet for public c
+### Subnet for public c
 resource "aws_subnet" "public_c" {
-  vpc_id                 = aws_vpc.example.id
-  cidr_block             = "var.vpc.subnet.public_c.cidr"
-  map_public_ip_on_launc = true
-  availability_zone      = "ap-northeast-1c"
+  vpc_id                 = aws_vpc.vpc.id
+  cidr_block             = local.subnet_public_c_cidr
+  map_public_ip_on_launch = true
+  availability_zone      = local.subnet_c_az
+
+  tags = {
+    Name = local.subnet_public_name_c
+  }
 }
 
-## Internet Gateway
-### Create Internet Gateway for VPC
-resource "aws_internet_gateway" "example" {
-  vpc_id = "aws_vpc.exapmple.id"
+### Subnet for protected a
+resource "aws_subnet" "protected_a" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = local.subnet_protected_a_cidr
+  availability_zone       = local.subnet_a_az
+  map_public_ip_on_launch = false
 
+  tags = {
+    Name = local.subnet_protected_name_a
+  }
 }
 
-## Route Table for Public Network
-### Create Route Table for Public
+### Subnet for protected c
+resource "aws_subnet" "protected_c" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = local.subnet_protected_c_cidr
+  availability_zone       = local.subnet_c_az
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = local.subnet_protected_name_c
+  }
+}
+
+### Subnet for private a
+resource "aws_subnet" "private_a" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = local.subnet_private_a_cidr
+  availability_zone       = local.subnet_a_az
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = local.subnet_private_name_a
+  }
+}
+
+### Subnet for private c
+resource "aws_subnet" "private_c" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = local.subnet_private_c_cidr
+  availability_zone       = local.subnet_c_az
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = local.subnet_private_name_c
+  }
+}
+
+## Route Table
+### Route Table for Public
 resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.example.id
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = local.route_table_public_name
+  }
 }
 
-### Create Routing For Public Network
-resource "aws_route" "public" {
-  route_table_id         = aws_route_table.public.id
-  gateway_id             = aws_internet_gateway.example.id
-  destination_cidr_block = "0.0.0.0/0"
+### Route Table for Protected using a 2 NAT Gateways
+resource "aws_route_table" "protected_a" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = local.route_table_protected_name_a
+  }
 }
 
-### Associetion Route Table for public subnets
-resource "aws_route_table_association" "public" {
-  subnet_id = [
-    aws_subnet.public_a.id,
-    aws_subnet.public_c.id
-  ]
+resource "aws_route_table" "protected_c" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = local.route_table_protected_name_a
+  }
+}
+
+### Route Table for Private
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = local.route_table_private_name
+  }
+}
+
+## Associetion Route Table
+### public a 
+resource "aws_route_table_association" "public_a" {
+  
+  subnet_id = aws_subnet.public_a.id
   route_table_id = aws_route.public.id
 }
 
-## Subnet for Protected Network
-### Create Subnet for protected a
-resource "aws_subnet" "protected_a" {
-  vpc_id                  = aws_vpc.example.id
-  cidr_block              = "var.vpc.subnet.protected_a.cidr"
-  availability_zone       = "ap-northeast-1a"
-  map_public_ip_on_launch = false
+### public c
+resource "aws_route_table_association" "public_c" {
+  subnet_id = aws_subnet.public_c.id
+  route_table_id = aws_route.public.id
 }
 
-### Create Subnet for protected c
-resource "aws_subnet" "protected_c" {
-  vpc_id                  = aws_vpc.example.id
-  cidr_block              = "var.vpc.subnet.protected_c.cidr"
-  availability_zone       = "ap-northeast-1c"
-  map_public_ip_on_launch = false
+
+### protected
+resource "aws_route_table_association" "protected_a" {
+  subnet_id      = aws_subnet.protected_a.id
+  route_table_id = aws_route_table.protected_a.id
 }
 
-### Create Route Table for Protected Network
-resource "aws_route_table" "protected" {
-  vpc_id = aws_vpc.example.id
+resource "aws_route_table_association" "protected_c" {
+  subnet_id      = aws_subnet.protected_c.id
+  route_table_id = aws_route_table.protected_c.id
 }
 
-#### Case of separate routing a and c, using a 2 NAT Gateways
-# resource "aws_route_table" "protected_a" {
-#   vpc_id = aws_vpc.example.id
-# }
-
-# resource "aws_route_table" "protected_c" {
-#   vpc_id = aws_vpc.example.id
-# }
-
-### Associetion Route Table for public subnets
-resource "aws_route_table_association" "protected" {
-  subnet_id = [
-    aws_vpc_subnet.protected_a.id,
-    aws_vpc_subnet.protected_c.id
-  ]
-  route_table_id = aws_route_table.protected.id
-}
-
-### Create EIP for NAT Gateway
+## NAT GW
+### EIP for NAT Gateway
 resource "aws_eip" "nat_gateway_a" {
   vpc = true
   depends_on = [
-    aws_internet_gateway.example
+    aws_internet_gateway.ig
   ]
 }
 
-# resource "aws_eip" "nat_gateway_c" {
-#   vpc = true
-#   depends_on = [
-#     aws_internet_gateway.example
-#   ]
-# }
+resource "aws_eip" "nat_gateway_c" {
+  vpc = true
+  depends_on = [
+    aws_internet_gateway.ig
+  ]
+}
 
-### Create NAT Gateway in AZ a
-resource "aws_nat_gateway" "example_a" {
+### NAT Gateway in AZ a
+resource "aws_nat_gateway" "nat_gw_a" {
   allocation_id = aws_eip.nat_gateway_a.id
   subnet_id     = aws_subnet.public_a.id
   depends_on = [
-    aws_internet_gateway.example
+    aws_internet_gateway.ig
   ]
 }
 
-####  Create NAT Gateway in AZ c
-# resource "aws_nat_gateway" "example_c" {
-#   allocation_id = aws_eip.nat_gateway_c.id
-#   subnet_id     = aws_subnet.public_c.id
-#   depends_on = [
-#     aws_internet_gateway.example
-#   ]
-# }
+###  NAT Gateway in AZ c
+resource "aws_nat_gateway" "nat_gw_c" {
+  allocation_id = aws_eip.nat_gateway_c.id
+  subnet_id     = aws_subnet.public_c.id
+  depends_on = [
+    aws_internet_gateway.ig
+  ]
+}
 
-### Create Routing For Protected Network using a NAT Gateway
-resource "aws_route" "protected" {
-  route_table_id         = aws_route_table.protected.id
-  nat_gateway_id         = aws_nat_gateway.example_a.id
+## Routing
+### Routing For Public Network
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = aws_internet_gateway.ig.id
   destination_cidr_block = "0.0.0.0/0"
 }
 
-#### Case of separate route table a and c
-# resource "aws_route" "protected_a" {
-#   route_table_id         = aws_route_table.protected_a.id
-#   nat_gateway_id         = aws_nat_gateway.example_a.id
-#   destination_cidr_block = "0.0.0.0/0"
-# }
+### Routing For Protected Network using a NAT Gateway
+resource "aws_route" "protected_a" {
+  route_table_id         = aws_route_table.protected_a.id
+  nat_gateway_id         = aws_nat_gateway.nat_gw_a.id
+  destination_cidr_block = "0.0.0.0/0"
+}
 
-# resource "aws_route" "protected_c" {
-#   route_table_id         = aws_route_table.protected_c.id
-#   nat_gateway_id         = aws_nat_gateway.example_c.id
-#   destination_cidr_block = "0.0.0.0/0"
-# }
+resource "aws_route" "protected_c" {
+  route_table_id         = aws_route_table.protected_c.id
+  nat_gateway_id         = aws_nat_gateway.nat_gw_c.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+### Routing For Private Network
+# Noting to do, only initial route table.
---
+++ modules/vpc/variables.tf
@@ -0,0 +1,37 @@
+locals {
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "default"
+  region                   = "ap-northeast-1"
+  account_id               = data.aws_caller_identity.current.account_id
+
+  # Names
+  vpc_name                     = "VPC01"
+  ig_name                      = "IG01"
+  subnet_public_name_a         = "public_subnet_a"
+  subnet_public_name_c         = "public_subnet_c"
+  subnet_protected_name_a      = "protected_subnet_a"
+  subnet_protected_name_c      = "protected_subnet_c"
+  subnet_private_name_a        = "private_subnet_a"
+  subnet_private_name_c        = "private_subnet_c"
+  route_table_public_name      = "public_route_table"
+  route_table_protected_name_a = "protected_route_table_a"
+  route_table_protected_name_c = "protected_route_table_c"
+  route_table_private_name     = "private_route_table"
+
+  vpc_cidr                = "172.16.0.0/16"
+  vpc_dns_support         = true
+  vpc_dns_hostname        = true
+  subnet_public_a_cidr    = "172.16.0.0/24"
+  subnet_public_c_cidr    = "172.16.1.0/24"
+  subnet_protected_a_cidr = "172.16.2.0/24"
+  subnet_protected_c_cidr = "172.16.3.0/24"
+  subnet_private_a_cidr   = "172.16.4.0/24"
+  subnet_private_c_cidr   = "172.16.5.0/24"
+  subnet_a_az             = "ap-northeast-1a"
+  subnet_c_az             = "ap-northeast-1c"
+  route_table_public      = "puclic"
+  route_table_protected   = "protected"
+  route_table_private     = "private"
+  env                     = "dev"
+}
