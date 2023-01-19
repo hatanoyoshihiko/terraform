c8e405d59ca79eebed73dbed1b546e333944a534
Hatano Yoshihiko <66893746+hatanoyoshihiko@users.noreply.github.com>
2023-01-19 22:01:39 +0900

    add variables for module

--- modules/security_group/variables.tf
+++ modules/security_group/variables.tf
@@ -10,7 +10,21 @@ locals {
 
 variable "name" {}
 variable "vpc_id" {}
-variable "port" {}
-variable "cidr_blocks" {
+variable "ingress_from_port" {}
+variable "ingress_to_port" {}
+variable "ingress_protocol" {}
+variable "ingress_ipv4_cidr_blocks" {
+  type = list(string)
+}
+variable "ingress_ipv6_cidr_blocks" {
+  type = list(string)
+}
+variable "egress_from_port" {}
+variable "egress_to_port" {}
+variable "egress_protocol" {}
+variable "egress_ipv4_cidr_blocks" {
+  type = list(string)
+}
+variable "egress_ipv6_cidr_blocks" {
   type = list(string)
 }
