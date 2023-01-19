0409904bc4c1e0a5cfd440f32a87a7439698bc20
Hatano Yoshihiko <66893746+hatanoyoshihiko@users.noreply.github.com>
2023-01-19 22:02:19 +0900

    improved  security group rule

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -14,22 +14,37 @@ data "aws_caller_identity" "current" {}
 resource "aws_security_group" "default" {
   name   = var.name
   vpc_id = var.vpc_id
-}
 
-resource "aws_security_group_rule" "ingress" {
-  type              = "ingress"
-  from_port         = var.port
-  to_port           = var.port
-  protocol          = "tcp"
-  cidr_blocks       = var.cidr_blocks
-  security_group_id = aws.security_group_id.default.id
-}
+  ingress {
+    from_port        = var.ingress_from_port
+    to_port          = var.ingress_to_port
+    protocol         = var.ingress_protocol
+    cidr_blocks      = var.ingress_ipv4_cidr_blocks
+    ipv6_cidr_blocks = var.ingress_ipv6_cidr_blocks
+  }
 
-resource "aws_security_group_rule" "egress" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws.security_group_id.default.id
+  egress {
+    from_port        = var.egress_from_port
+    to_port          = var.egress_to_port
+    protocol         = var.egress_protocol
+    cidr_blocks      = var.egress_ipv4_cidr_blocks
+    ipv6_cidr_blocks = var.egress_ipv6_cidr_blocks
+  }
 }
+# resource "aws_security_group_rule" "ingress" {
+#   type              = "ingress"
+#   from_port         = var.port
+#   to_port           = var.port
+#   protocol          = "tcp"
+#   cidr_blocks       = var.cidr_blocks
+#   security_group_id = aws.security_group_id.default.id
+# }
+
+# resource "aws_security_group_rule" "egress" {
+#   type              = "egress"
+#   from_port         = 0
+#   to_port           = 0
+#   protocol          = "-1"
+#   cidr_blocks       = ["0.0.0.0/0"]
+#   security_group_id = aws.security_group_id.default.id
+# }
