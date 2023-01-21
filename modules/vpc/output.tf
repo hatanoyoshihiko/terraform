76c2b981cd72de874becf8792370cd35b324d0a7
Hatano Yoshihiko <66893746+hatanoyoshihiko@users.noreply.github.com>
2023-01-21 11:20:57 +0900

    add subnet id variables in output.tf

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -10,6 +10,30 @@ output "vpc_cidr_block" {
   value = aws_vpc.vpc.cidr_block
 }
 
+output "public_a_az" {
+  value = aws_subnet.public_a.id
+}
+
+output "public_c_az" {
+  value = aws_subnet.public_c.id
+}
+
+output "protected_a_az" {
+  value = aws_subnet.protected_a.id
+}
+
+output "protected_c_az" {
+  value = aws_subnet.protected_c.id
+}
+
+output "private_a_az" {
+  value = aws_subnet.private_a.id
+}
+
+output "private_c_az" {
+  value = aws_subnet.private_c.id
+}
+
 output "http_sg_id" {
   value = module.http_sg.id
 }
